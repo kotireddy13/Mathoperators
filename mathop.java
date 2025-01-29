24d9c3006ef1c40ced792c588ddc37882bf181d0
kotireddy13 <koteshwarreddygogula@gmail.com>
2025-01-29 22:41:21 +0530

    Add files via upload

---
+++ mathop.java
@@ -0,0 +1,12 @@
+public class mathop {
+    public static void main(String[] args) {
+        int a=50;
+        int b=79;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+
+    }
+}
